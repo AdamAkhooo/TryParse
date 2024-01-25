00b920e124b5934c8ee6f30e59b7355a5c70fd06
AdamAkhooo <adamjamal0505@gmail.com>
2024-01-25 11:26:46 +0100

    fhgdf

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program{
+    static void Main(string[]args){
+        
+        int result;
+        if(!TryParse(Console.ReadLine(), out result))
+        Console.WriteLine("Skriv in en siffra");
+        else{
+            Console.WriteLine(result);
+            }
+        
+        
+        
+        
+         static bool TryParse(string s, out int result){
+            result =int.Parse(s);
+            if(result != null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+}
